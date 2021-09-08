14c9a5ffeea6f57999d5761f1639765100617acb
dutiewx <90184919+dutiewx@users.noreply.github.com>
2021-09-08 16:52:54 +0300

    Add files via upload

---
+++ lab1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1c.sh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, r;
+            do
+            {
+                Console.Write("Введите h: \n");
+                h = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите r: ");
+                r = int.Parse(Console.ReadLine());
+            } while (h <= 0 | r <= 0);
+
+                double S = Math.PI * Math.Pow(r, 2);
+                double V = (S * h) / 3;
+                Console.WriteLine($"Обьем конуса равен {V}");            
+        }
+    }
+}
---
+++ lab3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab3c.sh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите 3 числа: ");
+            Console.WriteLine("Число а = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Число b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Число c = ");
+            int c = int.Parse(Console.ReadLine());
+            if(a==b)
+            {
+                Console.Write("Числа a и b совпадают ");
+            }
+            else if(b==c)
+            {
+                Console.Write("Числа b и c совпадают ");
+            }
+            else if (a==c)
+            {
+                Console.Write("Числа a и c совпадают ");
+            }
+            else if (a!=b && b!=c && a!=c)
+            {
+                Console.Write("Числа не совпадают ");
+            }
+        }
+    }
+}
