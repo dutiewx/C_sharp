b1b6544993bd066d935cb25c901ad6e012318ca8
dutiewx <90184919+dutiewx@users.noreply.github.com>
2021-09-08 18:12:05 +0300

    Add files via upload

---
+++ lab1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1c.sh
+{
+    class lab1
+    {
+        static void Main(string[] args)
+        {
+            double h, r;
+            do
+            {
+                Console.Write("Введите h: \n");
+                h = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите r: ");
+                r = int.Parse(Console.ReadLine());
+            } while (h <= 0 | r <= 0);
+
+            double S = Math.PI * Math.Pow(r, 2);
+            double V = (S * h) / 3;
+            Console.WriteLine($"Обьем конуса равен {V}");
+        }
+    }
+}
---
+++ lab3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3c.sh
+{
+    class lab3
+    {
+        static void Main(string[] args)
+        {
+
+            bool check = true;
+            Console.WriteLine("Напишите 3 числа: ");
+            Console.Write("Число а = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Число b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Число c = ");
+            int c = int.Parse(Console.ReadLine());
+            if (a == b | b == c | a == c)
+            {
+                Console.Write(check);
+            }
+            else if (a != b && b != c && a != c)
+            {
+                Console.Write(!check);
+            }
+        }
+    }
+}
